b4b1cb42c736f0c8c3e2dc43287cf4cc61ce22c2
andreyp <tchaikovsky@inbox.ru>
2015-04-30 18:39:29 +0300

    some code styling

--- js/gauge.js
+++ js/gauge.js
@@ -24,81 +24,51 @@ var gauge = function(parent){
   };
 
   function arc(innerRadius , outerRadius, startAngle , endAngle) {
-
-    var r0 =innerRadius,
-        r1 =outerRadius,
-        a0 = startAngle ,
-        a1 = endAngle,
-        cw = a0 > a1 ? 0 : 1;
-
+    cw = startAngle > endAngle ? 0 : 1;
+    path = [];
     // Ensure that the outer radius is always larger than the inner radius.
-    if (r1 < r0) rc = r1, r1 = r0, r0 = rc;
-   
-    var x0,
-        y0,
-        x1,
-        y1,
-        x2,
-        y2,
-        x3,
-        y3,
-        path = [];
+    if (outerRadius < innerRadius) rc = outerRadius, outerRadius = innerRadius, innerRadius = rc;
+    
+    x0 = outerRadius * Math.cos(startAngle);
+    y0 = outerRadius * Math.sin(startAngle);
+    x1 = outerRadius * Math.cos(endAngle);
+    y1 = outerRadius * Math.sin(endAngle);
 
-    // Compute the two outer corners.
-
-      x0 = r1 * Math.cos(a0);
-      y0 = r1 * Math.sin(a0);
-      x1 = r1 * Math.cos(a1);
-      y1 = r1 * Math.sin(a1);
-
-      // Detect whether the outer corners are collapsed.
-      var l1 = Math.abs(a1 - a0 ) <= Math.PI ? 0 : 1;
+    // Detect whether the outer corners are collapsed.
+    var l1 = Math.abs(endAngle - startAngle ) <= Math.PI ? 0 : 1;
    
     // Compute the two inner corners.
-      x2 = r0 * Math.cos(a1);
-      y2 = r0 * Math.sin(a1);
-      x3 = r0 * Math.cos(a0);
-      y3 = r0 * Math.sin(a0);
+    x2 = innerRadius * Math.cos(endAngle);
+    y2 = innerRadius * Math.sin(endAngle);
+    x3 = innerRadius * Math.cos(startAngle);
+    y3 = innerRadius * Math.sin(startAngle);
 
-      // Detect whether the inner corners are collapsed.
-      var l0 = Math.abs(a0 - a1 ) <= Math.PI ? 0 : 1;
-   
-      // Compute straight corners.
-      path.push("M", x0, ",", y0);
-      if (x1 != null) path.push("A", r1, ",", r1, " 0 ", l1, ",", cw, " ", x1, ",", y1);
-      path.push("L", x2, ",", y2);
-      if (x3 != null) path.push("A", r0, ",", r0, " 0 ", l0, ",", 1 - cw, " ", x3, ",", y3);
-     
-      path.push("Z");
-
-      
-      return path.join("");
-  };
-
-  function tick(innerRadius , outerRadius, startAngle) {
-    var x0,
-        y0,
-        x2,
-        y2,
-       
-        path = [];
-   
-    // Compute the two inner  corner.
-    x0 = innerRadius * Math.cos(startAngle);
-    y0 = innerRadius * Math.sin(startAngle);
-   
-
-    // Compute the two outer corner.
-    x2 = outerRadius * Math.cos(startAngle);
-    y2 = outerRadius * Math.sin(startAngle);
+    // Detect whether the inner corners are collapsed.
+    var l0 = Math.abs(startAngle - endAngle ) <= Math.PI ? 0 : 1;
     
+    // Compute straight corners.
     path.push("M", x0, ",", y0);
+    if (x1 != null) path.push("A", outerRadius, ",", outerRadius, " 0 ", l1, ",", cw, " ", x1, ",", y1);
     path.push("L", x2, ",", y2);
+    if (x3 != null) path.push("A", innerRadius, ",", innerRadius, " 0 ", l0, ",", 1 - cw, " ", x3, ",", y3);
+   
+    path.push("Z");
    
     return path.join("");
   };
 
-  
+  function tickLine(innerRadius , outerRadius, startAngle) {
+    // Compute the two inner  corner.
+    x0 = innerRadius * Math.cos(startAngle);
+    y0 = innerRadius * Math.sin(startAngle);
+   
+    // Compute the two outer corner.
+    x2 = outerRadius * Math.cos(startAngle);
+    y2 = outerRadius * Math.sin(startAngle);
+
+    return "M" + x0 + "," + y0 + "L" + x2 + "," + y2;
+  };
+
   function tickText(innerRadius ,  startAngle) {
     var x0,
         y0;
@@ -121,7 +91,7 @@ var gauge = function(parent){
     this.setLabels(settings.labels);
     this.setValue(settings.gaugeValue);
     return this;
-  }
+  };
 
   this.setColorScales = function(items){
     this.data.colorScales =[];
@@ -130,6 +100,7 @@ var gauge = function(parent){
     }
     return this;
   };
+
   this.setColorValues = function(items){
     this.data.colors =[];
     for(i in items){
@@ -137,6 +108,7 @@ var gauge = function(parent){
     }
     return this;
   };
+
   this.setLabels = function(items){
     this.data.labels =[];
     for(i in items){
@@ -144,49 +116,55 @@ var gauge = function(parent){
     }
     return this;
   };
+
   this.setValue = function(value) {
     this.gaugeValue = value/100;
-
     if (this.arrow != null){
       this.arrow.setAttribute('transform','rotate('+(this.startAngle + (this.endAngle - this.startAngle)*this.gaugeValue)+')');
     }
-
     return this;
   };
-  
-  
+    
   this.setGaugeRadius = function(r){
     this.gaugeRadius = r;
     return this;
   };
+
   this.setTickSize = function(s){
     this.tickSize = s;
     return this;
   };
+
   this.setLabelMargin = function(padding){
     this.labelMargin  = padding;
     return this;
-  }
+  };
+
   this.setTickMargin = function(padding){
     this.tickMargin  = padding;
     return this;
-  }
+  };
+
   this.setStartAngle = function(a){
     this.startAngle  = a;
     return this;
-  }
+  };
+
   this.setEndAngle = function(a){
     this.endAngle  = a;
     return this;
-  }
+  };
+
   this.setArrowLength = function(al){
     this.arrowLength = al;
     return this;
-  }
-   this.setGaugeThick = function(t){
+  };
+
+  this.setGaugeThick = function(t){
     this.gaugeThick = t;
     return this;
-  }
+  };
+
   this.render = function(){
     var rad=(Math.PI/180);
    
@@ -197,7 +175,6 @@ var gauge = function(parent){
     var g1 = document.createElementNS(svgNS,"g");       
     this.parent.appendChild(svg);
     
-
     arrowThick = 5;
     arrowBaseRad = 10;
 
@@ -223,7 +200,7 @@ var gauge = function(parent){
 
     for (i =0 ; i < this.data.labels.length; i++){
       angle = angleFrom + len*(i/total);
-      d = tick( this.gaugeRadius + this.tickMargin, this.gaugeRadius+this.tickMargin+this.tickSize, angle);
+      d = tickLine( this.gaugeRadius + this.tickMargin, this.gaugeRadius+this.tickMargin+this.tickSize, angle);
       line2 = document.createElementNS(svgNS,"path");
       line2.setAttribute('d', d);
       line2.setAttribute('class','gauge-tick');
@@ -244,17 +221,12 @@ var gauge = function(parent){
     
       delta = rad*10;
       if (this.tickSize >0 ){
-        
         text2.setAttribute('x', _tickText.x - w/2 + (w/2) *Math.cos(angle) ) ;
         text2.setAttribute('y', _tickText.y + h *(1+Math.sin(angle))/4) ;
-       
       }else{
-        
         text2.setAttribute('x', _tickText.x - w/2 - (w/2) *Math.cos(angle) ) ;
         text2.setAttribute('y', _tickText.y - (h) *Math.sin(angle) ) ;
-       
       }
-      
     }
     
     cir = document.createElementNS(svgNS,"circle");
@@ -281,41 +253,42 @@ var gauge = function(parent){
     return this;
   }
   
-  
 };
 
-(function ( $ ) {
-  $.fn.jqgauge = function(options){
-    var self = this;
-    this.defaults = {
-           gaugeRadius: 180,
-           gaugeThick: 3,
-           arrowLength: 175,
-           startAngle: 180-30,
-           endAngle:360+30,  
-           tickSize:5,
-           tickMargin:5,
-           labelMargin:5,
-           colorScales:[0,70,90, 100],
-           colorValues:['#666666', '#ffa500', '#ff0000'],
-           labels:['0','1','2','3','4','5','6','7','8','9','10'],
-           gaugeValue:100
-        }
-    this.settings = $.extend( {}, this.defaults, options );
-    this.each(function(){
-        this.instance = new gauge(this).setOptions(self.settings).render();
-    });
-    return this;
-   };
-   $.fn.setValue = function(val){
-    var self = this;
-    self.settings.gaugeValue = val/100;
-    this.each(function(){
-      $(this).find('.gauge-arrow').attr('transform','rotate('+(self.settings.startAngle + (self.settings.endAngle - self.settings.startAngle)*self.settings.gaugeValue)+')');
+if (window.jQuery) { 
+  (function ( $ ) {
+    $.fn.jqgauge = function(options){
+      var self = this;
+      this.defaults = {
+             gaugeRadius: 180,
+             gaugeThick: 3,
+             arrowLength: 175,
+             startAngle: 180-30,
+             endAngle:360+30,  
+             tickSize:5,
+             tickMargin:5,
+             labelMargin:5,
+             colorScales:[0,70,90, 100],
+             colorValues:['#666666', '#ffa500', '#ff0000'],
+             labels:['0','1','2','3','4','5','6','7','8','9','10'],
+             gaugeValue:100
+          }
+      this.settings = $.extend( {}, this.defaults, options );
+      this.each(function(){
+          this.instance = new gauge(this).setOptions(self.settings).render();
+      });
+      return this;
+     };
+     $.fn.setValue = function(val){
+      var self = this;
+      self.settings.gaugeValue = val/100;
+      this.each(function(){
+        $(this).find('.gauge-arrow').attr('transform','rotate('+(self.settings.startAngle + (self.settings.endAngle - self.settings.startAngle)*self.settings.gaugeValue)+')');
+        
+      });
       
-    });
-    
-    return this; 
-   }
-             
-}( jQuery ));
+      return this; 
+     }
+               
+  }( jQuery ));
+}
